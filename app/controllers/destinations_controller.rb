1c7952c874402269c0cda09d6fe9554854a92e2a
Ola <aleksandra.kiszka91@gmail.com>
2018-04-16 20:46:01 +0000

    Create a simple_form template for New Trip Destinations

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -4,4 +4,23 @@ class DestinationsController < ApplicationController
         
     end    
     
+    def new
+        @destination = Destination.new
+    end
+    
+    def create
+        @destination = Destination.new(destination_params)
+        
+        if @destination.save
+            redirect_to root_path
+        else
+            render 'new'
+        end    
+    end
+    
+    private 
+    
+    def destination_params
+       params.require(:destination).permit(:name, :country, :description) 
+    end    
 end
