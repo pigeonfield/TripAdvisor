7ad55eb2ccaf0e6669990656fd7747b7a7d25f65
Ola <aleksandra.kiszka91@gmail.com>
2018-04-22 10:16:11 +0000

    Correct the syntax mistake in the main app controller

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -7,7 +7,6 @@ class DestinationsController < ApplicationController
     end    
     
     def show
-
     end    
     
     def new
@@ -24,13 +23,32 @@ class DestinationsController < ApplicationController
         end    
     end
     
+    def edit
+    end
+    
+    def update
+        if @destination.update(destination_params)
+            redirect_to destination_path(@destination)
+        else
+            render 'edit'
+        end 
+    end
+    
+    
+    def destroy
+        @destination.destroy
+        redirect_to root_path
+    end    
+    
+    
     private 
     
-    def destination_params
-       params.require(:destination).permit(:name, :country, :description) 
-    end    
+        def destination_params
+           params.require(:destination).permit(:name, :country, :description) 
+        end    
+        
+        def find_destination
+            @destination = Destination.find(params[:id])        
+        end    
     
-    def find_destination
-        @destination = Destination.find(params[:id])        
-    end    
 end
