eb92db39f1ac1eeacde37a2c747e14097ebf1129
Ola <aleksandra.kiszka91@gmail.com>
2018-05-11 12:28:30 +0000

    Add before_action part in Rating controller

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,4 +1,6 @@
 class RatingsController < ApplicationController
+    before_action :find_destination
+    
     
     def new
         @rating = Rating.new
@@ -6,6 +8,9 @@ class RatingsController < ApplicationController
     
     def create
         @rating = Rating.new(rating_params)
+        @rating.destination_id = @destination.id
+        @rating.redactor_id = current_redactor.id
+        
                
         if @rating.save
             redirect_to destination_path(@destination)
@@ -18,5 +23,9 @@ class RatingsController < ApplicationController
     
     def rating_params
        params.require(:review).permit(:rating, :comment)
+    end    
+    
+    def find_destination
+        @destination = Destination.find(params[:destination_id])        
     end    
 end
